d6f993ad49212ce12027c1aa8caea79cebdcf50c
naoyamaguchi <naoyamaguchi0128@gmail.com>
2018-03-31 16:19:00 +0900

    add -c option

--- head.go
+++ head.go
@@ -9,28 +9,44 @@ import (
 
 var (
 	n = flag.Int("n", 10, "lines")
+	c = flag.Int("c", 0, "bytes")
 )
 
+const BUFSIZE = 1024
+
 func head() error {
-	if len(os.Args) < 1 {
+	if len(os.Args) < 2 {
 		return fmt.Errorf("Usage: head [-n lines | -c bytes] [file ...]")
 	}
 
 	flag.Parse()
 	filename := flag.Args()
 
-	openedfile, err := os.Open(filename[0])
-	if err != nil {
-		return fmt.Errorf("Can't open file: %s", filename)
-	}
-	defer openedfile.Close()
+	for _, filename := range filename {
+		openedfile, err := os.Open(filename)
+		if err != nil {
+			return fmt.Errorf("Can't open file: %s", filename)
+		}
+		defer openedfile.Close()
 
-	line := bufio.NewScanner(openedfile)
-	for i := 1; i < (*n+1) && line.Scan(); i++ {
-		fmt.Printf("%d:%s\n", i, line.Text())
+		line := bufio.NewScanner(openedfile)
+		fmt.Println("==> " + filename + " <==")
+		if *c != 0 {
+			buf := make([]byte, BUFSIZE)
+			for {
+				_, err := openedfile.Read(buf)
+				if err != nil {
+					break
+				}
+				fmt.Println(string(buf[:*c]))
+			}
+		} else {
+			for j := 1; j < (*n+1) && line.Scan(); j++ {
+				fmt.Printf("%d:%s\n", j, line.Text())
+			}
+		}
 	}
-
-	return line.Err()
+	return nil
 }
 
 func main() {
@@ -39,9 +55,3 @@ func main() {
 		os.Exit(1)
 	}
 }
-
-/*
-NaonoiMac:head nao$ head --help
-head: illegal option -- -
-usage: head [-n lines | -c bytes] [file ...]
-*/
